636fe00a98a8bd4bd36105babd00c850b12fcb87
anderconsu <anderconsu@gmail.com>
2023-08-01 18:35:06 +0200

    added logo gif for response wait

--- Frontend/src/gpt/response.jsx
+++ Frontend/src/gpt/response.jsx
@@ -1,31 +1,45 @@
 import { useState } from "react";
 
 const GptForm = () => {
-    const [message, setMessage] = useState('');
-    const [response, setResponse] = useState('');
+    const [message, setMessage] = useState("");
+    const [response, setResponse] = useState("");
     const [loading, setLoading] = useState(false);
 
     const getMessage = async (e) => {
         e.preventDefault();
         console.log(message);
         setLoading(true);
-        let response = await fetch(`http://localhost:3006/api/gpt/?message=${message}`);
+        let response = await fetch(
+            `http://localhost:3006/api/gpt/?message=${message}`
+        );
         console.log(response);
         let data = await response.text();
         console.log(data);
         setResponse(data);
         setLoading(false);
-        
-    }
+    };
     return (
         <section>
             <form onSubmit={getMessage}>
-                <input type="text" onChange={(e) => setMessage(e.target.value)} />
-                <button type="submit" disabled={loading}>Submit</button>
+                <input
+                    type="text"
+                    onChange={(e) => setMessage(e.target.value)}
+                />
+                <button type="submit" disabled={loading}>
+                    Submit
+                </button>
+                {loading ? (
+                    <picture>
+                        <img
+                            src="images/Green_Line_Branch_Organic_Nature_Logo.gif"
+                            alt="loading"
+                        />
+                    </picture>
+                ) : null}
                 <p>{response}</p>
             </form>
         </section>
-    )
-}
+    );
+};
 
-export default GptForm
+export default GptForm;
