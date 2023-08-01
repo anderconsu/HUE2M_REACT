b108ed34b3a9067484e51713efb227561d402106
anderconsu <anderconsu@gmail.com>
2023-08-01 17:27:51 +0200

    translate example removed, no needed

--- Frontend/src/main/translate.mjs
+++
@@ -1,5 +0,0 @@
-import { translate } from '@vitalets/google-translate-api';
-
-const text = await translate('Hola, que tal?', { to: 'en', from:'es' });
-
-console.log(text) // => 'Hello World! How are you?'
