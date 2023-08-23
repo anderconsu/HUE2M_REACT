f1ae1a00ed4fb04658a67dc4aa2e87b04ca75d60
anderconsu <anderconsu@gmail.com>
2023-08-23 19:14:47 +0200

    gpt con recomendacion personalizada

--- Backend/src/controllers/user/userController.js
+++ Backend/src/controllers/user/userController.js
@@ -28,6 +28,9 @@ const addUser = async (req, res) => {
 const getUser = async (req, res) => {
     try {
         let user = await User.findOne({ email: req.body.email });
+        if (!user) {
+            res.status(404).send();
+        }
         res.status(200).json(user);
     } catch (error) {
         res.status(400).json(error);
--- Frontend/src/components/gpt/response.jsx
+++ Frontend/src/components/gpt/response.jsx
@@ -3,15 +3,32 @@ import { useState, useContext } from "react";
 //Context
 import UserContext from "../../context/userContext";
 import IsDataContext from "../../context/isData";
+
+// Diccionario de condiciones
+const conditionDictionary = {
+    celiac: "celiaquia",
+    diabetes: "diabetes",
+    hypertension: "hipertension",
+    highColestherol: "colesterol alto",
+    lactoseintolerant: "intolerancia a la lactosa",
+    nutA: "alergia a los frutos secos",
+    eggA: "alergia a los huevos",
+    sojaA: "alergia a la soja"
+}
+
 const GptForm = () => {
     const { user, setUser } = useContext(UserContext); 
     const {isData, setIsData} = useContext(IsDataContext);
     const [response, setResponse] = useState("");
     const [loading, setLoading] = useState(false);
+    const [error, setError] = useState("");
 
     const getMessage = async (e) => {
-        setResponse("");
         e.preventDefault();
+        // Setters
+        setResponse("");
+        setError("");
+        // Get from local storage
         const formattedData = JSON.parse(localStorage.getItem("formattedData"));
         if (formattedData) {
             // Get from local storage
@@ -22,8 +39,53 @@ const GptForm = () => {
             //
             // TODO: get name (age, gender, weight, food preference and height) 
             let name = user.displayName
+            let userData = {}
+            try {
+                const response = await fetch("http://localhost:3006/user/get", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({email: user.email})   
+                });
+                if (!response.ok) {
+                    console.log(response);
+                    if (response.status === 404) {
+                        setError("No hay datos de tu usuario.")
+                        return
+                    }
+                }else{
+                    userData = await response.json();
+                }
+            } catch (error) {
+                console.log("error en getUser", error);
+                return
+            }
+            // Data extract
+            const height = userData.height;
+            const weight = userData.weight;
+            const birthday = userData.birthday;
+            let sex = userData.sex;
+            let foodPreference = userData.foodPreference;
+            const condition = userData.condition;
+            // Conditionals in data
+            if(sex =="M"){sex = "un hombre"}else if(sex == "H"){sex = "una mujer"}else{sex = null}
+            if(foodPreference == "Vege"){foodPreference = "vegetariano"}else if(foodPreference =="Vega"){foodPreference = "vegano"}else{foodPreference = null}
+
+            // User data messages
+            const sexMessage = sex ? `, soy ${sex}` : "";
+            const ageMessage = birthday ? `, tengo ${now.getFullYear() - birthday.split("-")[0]} años`: "";
+            const heightMessage = height ? `, mido ${height / 100}m` : "";
+            const weightMessage = weight ? `, peso ${weight}Kg` : "";
+            const foodPreferenceMessage = foodPreference ? `, soy ${foodPreference}` : "";
+            let conditionMessage = condition ? condition.map((con) => ` ${conditionDictionary[con]},`) : "";
+            if (conditionMessage){
+                conditionMessage = conditionMessage.join("");
+                conditionMessage = ` y tengo las siguientes condiciones medicas:${conditionMessage}.`
+            }
+
             // Message
-            let message = `Hola, me llamo ${name}, mido 1,70 , peso 70Kg y soy vegetariano, son las (${currentTime}), ten en cuenta que depende de la hora, puede que me quede alguna comida mas por hacer. He comido los siguientes ingredientes: --- ${ingListMessage.join(", ")} --- los cuales tienen los siguientes valores nutricionales:--- ${formattedData.join(", ")} --- respondeme diciendome que te parece la ingesta de hoy, si deberia controlar algo de la misma y recomendaciones para mejorarla. Si crees que todavia tengo una comida por hacer en el dia, recomiendame que comer.`;
+            let message = `Hola, me llamo ${name}${sexMessage}${ageMessage}${heightMessage}${weightMessage}${foodPreferenceMessage}, son las (${currentTime}), ten en cuenta que depende de la hora, puede que me quede alguna comida mas por hacer. He comido los siguientes ingredientes: --- ${ingListMessage.join(", ")} --- los cuales tienen los siguientes valores nutricionales:--- ${formattedData.join(", ")} --- respondeme diciendome que te parece la ingesta de hoy, si deberia controlar algo de la misma y recomendaciones para mejorarla. Si crees que todavia tengo una comida por hacer en el dia, recomiendame que comer.`;
             console.log(message);
             // Loading
             setLoading(true);
@@ -58,6 +120,7 @@ const GptForm = () => {
                 ) : null}
             </form>
             )}
+            {error && <p>{error}</p>}
         </section>
     );
 };
