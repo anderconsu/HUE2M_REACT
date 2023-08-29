ab4dbfde3d37bdcc6d61d6d946d362462f234eae
anderconsu <anderconsu@gmail.com>
2023-08-29 15:27:39 +0200

    middleware updated to check user token ownership

--- Backend/src/Middleware/authMiddleware.js
+++ Backend/src/Middleware/authMiddleware.js
@@ -5,16 +5,35 @@ const authMiddleware = async (req, res, next) => {
     try {
         console.log("auth entered");
         const idToken = req.headers.authorization;
+        let uid = ""
 
-        getAuth()
+        await getAuth()
             .verifyIdToken(idToken)
             .then((decodedToken) => {
-                next();
+                uid = decodedToken.uid;
+                console.log("token valido");
             })
             .catch((error) => {
                 res.status(401).send();
                 // Handle error
                 console.log("error en signInWithCredential", error);
+            });
+        await getAuth()
+            .getUser(uid)
+            .then((userRecord) => {
+                let user = userRecord.toJSON()
+              // See the UserRecord reference doc for the contents of userRecord.
+                console.log(`Successfully fetched user data`);
+                if (user.email === req.body.email) {
+                    console.log("Authorized email");
+                    next();
+                }else{
+                    res.status(401).json({ error: "Unauthorized request" });
+                }
+                
+            })
+            .catch((error) => {
+                console.log('Error fetching user data:', error);
             });
         
     } catch (error) {
